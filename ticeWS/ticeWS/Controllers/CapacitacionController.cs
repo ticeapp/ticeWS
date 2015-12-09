5785e9de45416efbd77fa626cc4cf72bb999a248
Milton Yarleque <milton.yarleque@gmail.com>
2015-12-08 21:35:11 -0500

    ws
    ws

---
+++ ticeWS/ticeWS/Controllers/CapacitacionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ticeWS.Controllers
+{
+    public class CapacitacionController : ApiController
+    {
+        //ticeEntities objapi = new ticeEntities();
+
+
+        //[HttpGet]
+        //public int  crearCapacidad(string nombre,bool certificado, int codigoPerido,string descripcion, string correoContacto, bool enviarNotificacion,int periodicidadEnvio, DateTime fechaInicio, bool capacitacionActiva, int codigoTaller, DateTime fechaCreacion, DateTime fechaModificacion, string usuarioCreacion, string usuarioModificacion, string lugar, DateTime fechaInicioEnvio,  DateTime fechaCapacitacion )
+        //{
+
+        //    return objapi.SP_CAPACITACION_CREATE(nombre, certificado, codigoPerido, descripcion, correoContacto, enviarNotificacion, periodicidadEnvio, fechaInicio, capacitacionActiva, codigoTaller, fechaCreacion, fechaModificacion, usuarioCreacion, usuarioModificacion, lugar, fechaInicioEnvio, fechaCapacitacion);
+
+        //}
+
+
+
+        
+
+        
+
+
+    }
+}
